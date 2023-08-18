d5b1a3cd61c8c7f7e987ed3b50b1fa01a6f1f20d
afrique18 <vincentv1225@gmail.com>
2023-08-18 10:48:10 -0400

    Add data for app

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,48 @@
-// Bringing in the required import from 'react-router-dom'
-import { Outlet } from 'react-router-dom';
-import Nav from './components/NavTabs';
 
-function App() {
-  // The Outlet component will conditionally swap between the different pages according to the URL
-  return (
-    <>
-      <Nav />
-      <main className="mx-3">
-        <Outlet />
-      </main>
-    </>
-  );
+import React, {Component} from 'react';
+import { HashRouter, Route } from 'react-router-dom';
+import './App.css';
+
+import Header from './components/Header';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Footer from '/components/Footer';
+import Portfolio from './pages/Portfolio';
+
+Class App extends Component {
+  render() {
+    return (
+      <HashRouter basename='/'>
+        <div className='app'>
+          <header/>
+          <main>
+            <Route exact path='/' Component={Home} />
+            <Route exact path='/about' Component={About} />
+            <Route exact path='/contact' Component={Contact} />
+            <Route exact path='/portfolio' Component={Portfolio} />
+            </main>
+            <Footer />
+        </div>
+      </HashRouter>
+    );
+  }
 }
+// // Bringing in the required import from 'react-router-dom'
+// import { Outlet } from 'react-router-dom';
+// import Nav from './components/NavTabs';
+// import { Component } from 'react';
+
+// function App() {
+//   // The Outlet component will conditionally swap between the different pages according to the URL
+//   return (
+//     <>
+//       <Nav />
+//       <main className="mx-3">
+//         <Outlet />
+//       </main>
+//     </>
+//   );
+// }
 
 export default App;
