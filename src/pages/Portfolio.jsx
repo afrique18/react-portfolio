a5b06387ae73a38f4e451357bb5efb0cdbf0731d
afrique18 <vincentv1225@gmail.com>
2023-08-26 14:07:49 -0400

    Update portfolio data

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,15 +1,22 @@
 import React from "react";
+import Project from '../components/Project';
 
 
 // import Project1 from "../assets/images/project1.png";
 function Portfolio() {
+  const projects = [
+    { title: 'Project 1', img: 'proj1.jpg', deployed: '#', repo: '#' },
+  ];
     return (
-        <section class="portfolio">
-        <h1 id="portfolio">Potfolio Page</h1>
-        <p>
+        <div>
+        <h2>Potfolio</h2>
+        {projects.map((project, i) => (
+          <Project key={i} {...project} />
+        ))}
+        {/* <p>
           This is my portfolio.
-        </p>
-      </section>
+        </p> */}
+      </div>
         );
     }
 
