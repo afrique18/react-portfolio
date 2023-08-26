6d10adaec4c74e08780dcee9be576b53b47ff4d0
afrique18 <vincentv1225@gmail.com>
2023-08-26 14:12:23 -0400

    Update resume data

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -2,9 +2,17 @@ import React from "react";
 
 function Resume() {
   return (
-    <section>
-      <h1>Download Resume</h1>
-    </section>
+    <div>
+      <h2>Resume</h2>
+      <a href="my-resume.pdf" download> Download Resume</a>
+      <ul>
+        <li>Proficiency 1</li>
+        <li>Proficiency 2</li>
+        <li>Proficiency 3</li>
+        <li>Proficiency 4</li>
+        <li>Proficiency 5</li>
+      </ul>
+    </div>
   );
 }
 
