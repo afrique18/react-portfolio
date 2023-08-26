7a61431066cef3d29f29bbdb3351c6582c819b60
afrique18 <vincentv1225@gmail.com>
2023-08-26 15:09:41 -0400

    Add data to footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,65 @@
 
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 // import { HashRouter, Route } from 'react-router-dom';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import './App.css';
 
 import Header from './components/Header';
-import Home from './pages/Home';
+// import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
-// import Footer from './components/Footer';
+import Footer from './components/Footer';
 import Portfolio from './pages/Portfolio';
+import Resume from './pages/Resume';
 
 // Class App extends Component {
 //   render() {
 
 function App() {
+  const [ currentPage, setCurrentPage] = useState(About);
+  const renderPage = () => {
+    switch (currentPage) {
+      case 'Portfolio':
+      return <Portfolio />;
+      case 'Contact':
+      return <Contact />;
+      case 'Resume':
+      return <Resume />;
+      default:
+        return <About />;
+    }
+  };
     return (
-      <>
-      <BrowserRouter>
-      <Routes>
-        <div className='app'>
-          {/* <Header /> */}
-          <main>
-            <Route exact path='/' Component={Home} />
-            <Route exact path='/about' Component={About} />
-            <Route exact path='/contact' Component={Contact} />
-            <Route exact path='/portfolio' Component={Portfolio} />
-            </main>
-            {/* <Footer /> */}
-        </div>
-      </Routes>
-    </BrowserRouter>
-      </> 
+      <div>
+        <Header setCurrentPage={setCurrentPage} currentPage={currentPage} />
+        <main>{renderPage()}</main>
+        <Footer />
+      </div>
     );
-//   }
-}
+
+
+
+
+
+//       <>
+//       <BrowserRouter>
+//       <Routes>
+//         <div className='app'>
+//           {/* <Header /> */}
+//           <main>
+//             <Route exact path='/' Component={Home} />
+//             <Route exact path='/about' Component={About} />
+//             <Route exact path='/contact' Component={Contact} />
+//             <Route exact path='/portfolio' Component={Portfolio} />
+//             </main>
+//             {/* <Footer /> */}
+//         </div>
+//       </Routes>
+//     </BrowserRouter>
+//       </> 
+//     );
+// //   }
+
 // // Bringing in the required import from 'react-router-dom'
 // import { Outlet } from 'react-router-dom';
 // import Nav from './components/NavTabs';
@@ -51,5 +76,7 @@ function App() {
 //     </>
 //   );
 // }
+
+}
 
 export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <footer>
+            <a href="https://github.com/afrique18">Github</a>
+            <a href="https://linkedin.com/Username">linkedin</a>
+            <a href="https://twitter.com/Username">Twitter</a>
+        </footer>
+    );
+}
+
+export default Footer;
