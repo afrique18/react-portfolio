152438d6010761cafb1a17dd125d715a958ad23d
afrique18 <vincentv1225@gmail.com>
2023-08-26 13:16:36 -0400

    Add data to helpers

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,7 @@
+export function capitalizedFirstLetter(string) {
+  return string.charAT(0).toUpperCase() + string.slice(1);
+}
+
 export function validateEmail(email) {
   const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(String(email).toLowerCase());
