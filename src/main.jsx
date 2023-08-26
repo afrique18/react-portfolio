ad2801a16c211280597ff692888a81010d7cfe47
afrique18 <vincentv1225@gmail.com>
2023-08-26 13:24:14 -0400

    Link css files to main

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css'
+import '../src/app.css';
+import '../src/index.css';
 
 import App from './App';
 import Error from './pages/Error';
